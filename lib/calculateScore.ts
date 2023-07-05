8e757862e72f31692e188f5cd49344a10ae5c9df
Jerome Wong <jeromewongjh@gmail.com>
2023-07-06 01:40:11 +0800

    feat: score calculator

---
+++ lib/calculateScore.ts
@@ -0,0 +1,12 @@
+export function calculateScore(breakdown: number[]) {
+  const weights = [30, 20, 20, 15, 10, 5];
+
+  const totalScore = breakdown.reduce((acc, score, idx) => {
+    const weight = weights[idx] as number;
+    const calculatedScore =
+      idx === 4 ? Math.ceil((score / 3) * weight) : (score / 5) * weight;
+    return acc + calculatedScore;
+  }, 0);
+
+  return totalScore;
+}
