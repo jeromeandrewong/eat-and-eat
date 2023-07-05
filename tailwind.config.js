da9787366315b3336d31be522ffddd8c16d96d62
Jerome Wong <jeromewongjh@gmail.com>
2023-07-06 01:38:34 +0800

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+import { Config } from "tailwindcss";
+
+/** @type {import('tailwindcss').Config} \*/
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#F5EFE4",
+
+        secondary: "#32675E",
+        accent: "#E07E2E",
+        primaryText: "#18181b",
       },
     },
   },
   plugins: [],
-}
+};
