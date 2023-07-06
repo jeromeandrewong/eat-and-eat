77410bdf5d60fbe8272a3ea8175d2f6e17398fba
Jerome Wong <jeromewongjh@gmail.com>
2023-07-07 03:00:16 +0800

    feat: nav

---
+++ app/components/Nav.tsx
@@ -0,0 +1,28 @@
+import { NavbarItems } from "@/lib/navbarItems";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <div className="flex h-full min-h-full items-center justify-center gap-4 rounded-lg border border-zinc-600 bg-primary/80 py-1 shadow-md lg:w-12">
+      <div className="flex justify-evenly gap-2 lg:flex-col">
+        {NavbarItems.map((item, index) => {
+          return (
+            <Link href={item.slug} key={index}>
+              <button
+                key={index}
+                className="flex h-12  items-center justify-center"
+              >
+                <item.icon
+                  size="2rem"
+                  className="cursor-pointer rounded  bg-secondary/25 py-2  shadow duration-300 ease-in-out hover:scale-110 hover:bg-secondary/50 hover:shadow-xl"
+                />
+              </button>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ lib/navbarItems.ts
@@ -0,0 +1,25 @@
+import { FiCoffee, FiAward, FiAtSign, FiInfo } from "react-icons/fi";
+
+export const NavbarItems = [
+  {
+    name: "Home",
+    slug: "/",
+    icon: FiCoffee,
+  },
+
+  {
+    name: "About",
+    slug: "/about",
+    icon: FiInfo,
+  },
+  {
+    name: "Scoring",
+    slug: "/scoring",
+    icon: FiAward,
+  },
+  {
+    name: "Contact",
+    slug: "/contact",
+    icon: FiAtSign,
+  },
+];
